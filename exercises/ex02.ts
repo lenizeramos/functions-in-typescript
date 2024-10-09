1e0e398f77b3bed3c9c1c1caecd0aa726d042866
lenizeramos <lenizemr@gmail.com>
2024-10-08 23:46:54 -0700

    wip

--- exercises/ex02.ts
+++ exercises/ex02.ts
@@ -1,13 +1,15 @@
 // Create an anonymous function that takes a string parameter and print the characters of the string in reverse order.
 // Return the string.
 
-const reverseAndPrint = () => {
-  // Code here
+const reverseAndPrint = (str: string): string =>{
+  return str.split("").reverse().join("")
 };
 
 // Tests:
 reverseAndPrint("hello"); // Expected output: "olleh"
 reverseAndPrint("world"); // Expected output: "dlrow"
 reverseAndPrint("12345"); // Expected output: "54321"
+
+
 
 export default reverseAndPrint;
--- exercises/ex03.ts
+++ exercises/ex03.ts
@@ -2,12 +2,17 @@
 // Use function overloading to define the function in two variants: one for numbers and another for strings.
 // Return the string.
 
-const combine = () => {
-  //Code here
-};
+function combine(param1: number, param2: number): string;
+function combine(param1: string, param2: string): string;
+function combine(param1: number, param2: string): string;
+function combine(param1: string, param2: number): string;
+function combine(param1: number | string, param2: number | string): string {
+  return `${param1}${param2}`;
+}
 
 console.log(combine(2, 3)); // Expected output: "23"
 console.log(combine("Hello, ", "world!")); // Expected output: "Hello, world!"
 console.log(combine(5, " days")); // Expected output: "5 days"
+console.log(combine("Error: ", 404));
 
 export default combine;
